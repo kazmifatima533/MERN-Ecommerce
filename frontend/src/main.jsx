5f585ae3ac4e0b045f8680465607b492b5409a37
kazmifatima533 <fatimakazmi533@gmail.com>
2025-05-28 13:17:42 +0500

    code upfsyrf

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,6 +3,7 @@ import App from './App.jsx'
 import './index.css'
 import { BrowserRouter } from 'react-router-dom'
 import ShopContextProvider from './context/ShopContext.jsx'
+import 'react-media-recorder'
 
 createRoot(document.getElementById('root')).render(
   <BrowserRouter>
